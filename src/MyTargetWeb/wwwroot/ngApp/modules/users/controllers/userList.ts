c7600060dfa6b25bde075898313b72c9750120d7
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2015-11-25 17:19:29 +0100

    add typings

--- src/MyTargetWeb/wwwroot/ngApp/modules/users/controllers/userList.ts
+++ src/MyTargetWeb/wwwroot/ngApp/modules/users/controllers/userList.ts
@@ -1,7 +1,7 @@
 export default class UserList{
   model : ng.resource.IResourceArray<arbor.users.IUser>;
-  get selectedUser() { return this.model.filter((u) => u.$selected)[0]; }
-  get isSelected()  { return !!this.selectedUser; }
+  get selectedUser() : arbor.users.IUser { return this.model.filter((u) => u.$selected)[0]; }
+  get isSelected() : boolean  { return !!this.selectedUser; }
 
   select(user : arbor.users.IUser){
     this.model.forEach((item : arbor.ui.ISelectable) => {
