a4b0610624f1fc9b18a2c150b57a9d9f13eac552
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2015-11-27 16:20:30 +0100

    helder

--- src/MyTargetWeb/gulpfile.js
+++ src/MyTargetWeb/gulpfile.js
@@ -1,4 +1,6 @@
-var gulp = require('gulp'), ts = require('gulp-typescript'), ngAnnotate = require('gulp-ng-annotate');
+var gulp = require('gulp'),
+ts = require('gulp-typescript'),
+ngAnnotate = require('gulp-ng-annotate');
 
 var tsConfig = require('./tsconfig.json').compilerOptions;
 
@@ -13,13 +15,22 @@ gulp.task('ts:watch', ['ts:compile'], function(){
 });
 
 gulp.task('tsx:compile', function(){
-  gulp.src(['./typings/**/*.d.ts', './wwwroot/ngApp/**/*.d.ts','./wwwroot/retApp/**/*.tsx'])
+  gulp.src(['./typings/**/*.d.ts', './wwwroot/ngApp/**/*.d.ts','./wwwroot/retApp/**/*.ts?'])
   .pipe(ts(tsConfig))
   .pipe(gulp.dest('./wwwroot/retApp'));
 });
 
 gulp.task('tsx:watch', ['tsx:compile'], function(){
-  gulp.watch('./wwwroot/retApp/**/*.tsx', ['tsx:compile']);
+  gulp.watch('./wwwroot/retApp/**/*.ts?', ['tsx:compile']);
+});
+
+gulp.task('less:compile', function(){
+  var c = require('gulp-concat-css');
+  gulp.src('./wwwroot/less/**/*.less')
+   .pipe(require('gulp-less'))
+  //.pipe(c('site.min.css'))
+  // .pipe(require('gulp-minify-css'))
+  .pipe(gulp.dest('./wwwroot'))
 });
 
 //jspm bundle ngApp/app wwwroot/bundle.angular.min.js --minify --no-mangle
--- src/MyTargetWeb/wwwroot/retApp/components/homePage.tsx
+++ src/MyTargetWeb/wwwroot/retApp/components/homePage.tsx
@@ -4,7 +4,7 @@ export default class HomePage extends React.Component<any, any> {
   render(){
     return (
       <div>
-        <nav className="navbar navbar-light bg-faded">
+        <nav id="navbar" className="navbar navbar-light bg-faded">
           <a className="navbar-brand" href="#">MyTarget</a>
           <ul className="nav navbar-nav">
             <li className="nav-item" ui-sref-active="active">
